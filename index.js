e39a19191efbaea81f8d684249f9e2a479511291
dominoking13 <143142557+dominoking13@users.noreply.github.com>
2025-01-12 22:03:02 -0500

    add the filter and update the css

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+        
 
     // use the function we previously created to add the unfunded games to the DOM
 
@@ -108,10 +108,10 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    const unfundedGames = GAMES_JSON.filter(game => game.pledged < game.goal);
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(unfundedGames);
 }
 
 // show all games
@@ -119,7 +119,8 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
+    
 }
 
 // select each button in the "Our Games" section
